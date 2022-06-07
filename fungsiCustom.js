b21b98a6ffd22da364366b1f0bc3c083fc27c8e4
gandatadyo <gandatadyosurya@gmail.com>
2022-06-07 21:33:26 +0700

    submit tugas

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,53 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let dataresult = [];
+
+  const readingFile1 = (callback, nextStep) => {
+    fs.readFile(file1, (err, data) => {
+      if (err) {
+        callback(err, dataresult)
+        return
+      }
+      const temp = JSON.parse(data)
+      dataresult.push(temp.message.split(' ')[1]);
+      nextStep(callback);
+    });
+  }
+  const readingFile2 = (callback, nextStep) => {
+    fs.readFile(file2, (err, data) => {
+      if (err) {
+        callback(err, dataresult)
+        return
+      }
+      const temp = JSON.parse(data)
+      if (temp.length > 0) {
+        dataresult.push(temp[0].message.split(' ')[1]);
+      };
+      nextStep(callback);
+    });
+  }
+  const readingFile3 = (callback) => {
+    fs.readFile(file3, (err, data) => {
+      if (err) {
+        callback(err, dataresult)
+        return
+      }
+      const temp = JSON.parse(data)
+      if (temp.length > 0) {
+        dataresult.push(temp[0].data.message.split(' ')[1]);
+      }
+      callback(null, dataresult);
+    });
+  }
+  readingFile1(fnCallback, (callback1) => {
+    readingFile2(callback1, (callback2) => {
+      readingFile3(callback2)
+    })
+  })
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
